5431bef01dc820cf7c1780837899f4abcd607d79
Sumona-Afrin-Joti <sumonajoti.06@gmail.com>
2021-08-21 20:00:11 +0600

    third-commit

--- add.js
+++ add.js
@@ -1,52 +1,24 @@
 const total = document.getElementById('total');
-const totalValue = parseFloat(total.innerText);
 const bonusTotal = document.getElementById('bonus-total');
 
-function updateMemoryCost(argument) {
-    const extraMemoryCost = document.getElementById('extra-memory-cost');
-    if (argument == '8GB') {
-        extraMemoryCost.innerText = 0;
+function updateCost(Id,price,update) {
+    const Cost = document.getElementById(Id);
+    if (update == false) {
+        Cost.innerText = price;
     }
-    else{
-        extraMemoryCost.innerText = 180;
-    }
-    getCost()
-}
-
-function updateExtraStorageCost(argument) {
-    const extraStorageCost = document.getElementById('extra-storage-cost');
-    
-    if (argument == 'storage1') {
-        extraStorageCost.innerText = 0;
-    }
-    else if (argument == 'storage2') {
-        extraStorageCost.innerText = 100;
-    }
-    else {
-        extraStorageCost.innerText = 180;
+    else if(update == true ){
+        Cost.innerText = price;
     }
     getCost()
-} 
-
-function updateDeliveryCost(argument) {
-    const deliveryCharge = document.getElementById('delivery-charge-cost');
-
-    if (argument == 'delivery-button-1') {
-        deliveryCharge.innerText = 0;
-    }
-    else {
-        deliveryCharge.innerText = 20;
-    }
-    getCost()
-  
 }
 
 function getCost() {
+    const bestCost = parseFloat(document.getElementById('best-cost').innerText);;
     const memoryCost = parseFloat(document.getElementById('extra-memory-cost').innerText);
     const storageCost = parseFloat(document.getElementById('extra-storage-cost').innerText);
     const deliveryCost = parseFloat(document.getElementById('delivery-charge-cost').innerText);
 
-    const totalPrice = 1299 + memoryCost + storageCost + deliveryCost;
+    const totalPrice = bestCost + memoryCost + storageCost + deliveryCost;
     total.innerText = totalPrice;
     bonusTotal.innerText = totalPrice;
 
